057adc0ad5e9fe341e0a12b711b97e0bf245340c
MatiasBernardini <bernardinimatias14@gmail.com>
2025-03-10 14:20:19 -0300

    modelo y ruta de orders agregado

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Order = require('../models/Order');
 
 router.post('/signup', async(req, res)=>{
     const {name, email, password} = req.body;
@@ -40,6 +41,15 @@ router.get('/', async (req, res) =>{
 })
 
 
+router.get('/:id/orders', async (req,res) => {
+    const {id} = req.params;
+    try {
+        const user = await User.findById(id).populate('orders');
+        res.json(user.orders);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,46 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const http = require('http');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
-require('./connection');
-const app = express();
+require('./connection')
 const server = http.createServer(app);
-const { Server } = require('socket.io');
+const {Server} = require('socket.io');
 const io = new Server(server, {
-  cors: '*',
-  methods: '*',
-});
+  cors: 'http://localhost:3001',
+  methods: ['GET', 'POST', 'PATCH', "DELETE"]
+})
+
 
 const User = require('./models/User');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 const imageRoutes = require('./routes/imageRoutes');
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 app.use('/images', imageRoutes);
 
-app.post('/create-payment', async (req, res) => {
-  const { amount } = req.body;
+
+app.post('/create-payment', async(req, res)=> {
+  const {amount} = req.body;
+  console.log(amount);
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: 'usd',
-      payment_method_types: ['card'],
+      payment_method_types: ['card']
     });
+    res.status(200).json(paymentIntent)
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json(e.message);
+   }
+})
 
-    res.status(200).json({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
 
-server.listen(8080, () => {
-  console.log('Servidor funcionando en puerto 8080');
-});
+server.listen(8080, ()=> {
+  console.log('server running at port', 8080)
+})
+
+app.set('socketio', io);
